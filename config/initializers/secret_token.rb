2f8d959a2b279078be8f77828f54129de1b8bfa2
fipa <fernandopobletearrau@gmail.com>
2011-11-09 19:48:58 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prototipo::Application.config.secret_token = '9ad408905e1fee415fdff6ff3eeeb6067f06407a8f05b83ebd45a485ae2a4072667aff7c4c3269f51b6d832914c5c98ff10b5bf4e01d4b3985f8307950649272'
